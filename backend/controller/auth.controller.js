358b40e124d4b47ea89c23984bbc81ea3857ed97
Kürşat Polatcı <polatcikursat58@gmail.com>
2024-10-03 14:12:26 +0300

    Logout Endpoint

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -84,3 +84,12 @@ export const verifyEmail = async (req, res) => {
         res.status(400).json({success: false, message: error.message})
     }
 }
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie("token");
+        res.status(200).json({success: true, message: "Logget out successfully"})
+    } catch (error) {
+        res.status(500).json({success: false, message: error.message})
+    }
+}
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from "express"
 
-import { signup, verifyEmail } from "../controller/auth.controller.js"
+import { logout, signup, verifyEmail } from "../controller/auth.controller.js"
 
 const router = express.Router();
 
 router.post("/signup", signup)
 router.post("/verify-email", verifyEmail)
+router.post("/logout", logout)
 
 export default router;
