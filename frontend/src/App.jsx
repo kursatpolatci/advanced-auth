ffdb950473d0aa9b13bf797ac3799e9c51d5babc
Kürşat Polatcı <polatcikursat58@gmail.com>
2024-10-03 19:36:11 +0300

    Frontend Setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom"
+
+import FloatingShape from "./components/FloatingShape"
+
+import SignUpPage from "./pages/SignUpPage"
+import LoginPage from "./pages/LoginPage"
+import Dashboard from "./pages/Dashboard"
 
 function App() {
 
   return (
-    <div className='text-center'>
-      <h1 className='text-red-500 text-3xl'>HOŞ GELDİN</h1>
+    <div className=" min-h-screen bg-gradient-to-br from-gray-800 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
+      <FloatingShape color="bg-green-500" size='w-64 h-64' top='-5%' left='10%' delay={0}/>
+      <FloatingShape color="bg-emerald-500" size='w-48 h-48' top='70%' left='80%' delay={5}/>
+      <FloatingShape color="bg-lime-500" size='w-32 h-32' top='40%' left='-10%' delay={2}/>
+
+      <Routes>
+        <Route path="/" element={<Dashboard />}/>
+        <Route path="/signup" element={<SignUpPage />}/>
+        <Route path="/login" element={<LoginPage />}/>
+      </Routes>
     </div>
   )
 }
